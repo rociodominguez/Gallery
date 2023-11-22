bb247e1dd77ed743ffc78da4b6641460d78b581f
Rocío Domínguez <rociodominguezcontacto@gmail.com>
2023-11-22 21:05:27 +0100

    first submission

--- main.js
+++ main.js
@@ -2,29 +2,36 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <h1>the gallery</h1>
-  <div id="exploreContainer"></div>
+  <h2>Unsplash: Your one-stop for stunning, free-to-use images from top photographers worldwide. Elevate your projects without worrying about royalties or attributions.</h2>
+  <p>Please enter the keyword in the field below to get related photos!</p>
+  <div id="exploreContainer">
+    <div class="InputContainer">
+      <input placeholder="e.g.: flowers, morning..." id="input" class="input" name="text" type="text">
+    </div>      
+  </div>
   <div id="imageContainer" class="container"></div>
+  <footer>
+    <div class="footer-content">
+      <p>&copy; 2023 The gallery</p>
+      <p>Created by <a target="_blank" href="https://www.linkedin.com/in/rociodomjim/">Rocío Domínguez</a></p>
+    </div>
+  </footer>
 `;
 
 const KEY = 'WYaoRls8J9S7BMmtRLUixQM4Ox2SJD9Ehl2Sr8m6iBQ';
 
+const imageContainer = document.getElementById('imageContainer');
+const inputValue = document.getElementById('input');
 const exploreContainer = document.getElementById('exploreContainer');
 
-const inputContainer = document.createElement('div');
-inputContainer.classList.add('InputContainer');
+const buttonExplore = document.createElement('button');
+buttonExplore.textContent = "search";
+buttonExplore.classList.add('exploreButton');
 
-const input = document.createElement('input');
-input.setAttribute('placeholder', 'Im looking for...');
-input.setAttribute('id', 'input');
-input.classList.add('input');
-input.setAttribute('name', 'text');
-input.setAttribute('type', 'text');
-
-inputContainer.appendChild(input);
-exploreContainer.appendChild(inputContainer);
+exploreContainer.appendChild(buttonExplore);
 
 const exploreImages = async () => {
-  const REQUEST = document.getElementById('input').value;
+  const REQUEST = inputValue.value;
   const PATH = `https://api.unsplash.com/search/photos?page=1&query=${REQUEST}&client_id=${KEY}`;
 
   try {
@@ -32,12 +39,11 @@ const exploreImages = async () => {
     const data = await response.json();
     showingImages(data.results);
   } catch (error) {
-    console.log(error);
+    console.log('error');
   }
 };
 
 const showingImages = (images) => {
-  const imageContainer = document.getElementById('imageContainer');
   imageContainer.innerHTML = '';
 
   images.forEach(image => {
@@ -48,3 +54,9 @@ const showingImages = (images) => {
 };
 
 buttonExplore.addEventListener('click', exploreImages);
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  inputValue.value = 'flowers';
+  exploreImages();
+});

87fbf30e661bde0aba1ba8137dd177f662870264
Rocío Domínguez <rociodominguezcontacto@gmail.com>
2023-11-23 18:27:31 +0100

    v1

--- components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-export const createSearchButton = () => {
-    const button = document.createElement('button');
-    button.textContent = 'search';
-    button.classList.add('exploreButton');
-    return button;
-  };
-  
--- components/Main/Main.js
+++
@@ -1,7 +0,0 @@
-import './Main.css'
-
-export const printMain = () => {
-    return `
-      <div id="imageContainer" class="container"></div>
-    `;
-  };  
